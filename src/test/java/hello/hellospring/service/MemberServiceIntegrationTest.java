1057e06d5caf7c8a681903874703355d056df091
expresshihway <03wow22@gmail.com>
2021-08-06 00:19:58 +0900

    [feat] 스프링 DB 접근기술: JDBC, JPA, Spring JPA

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -4,28 +4,31 @@ import hello.hellospring.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest
 @Transactional
 class MemberServiceIntegrationTest {
     @Autowired MemberService memberService;
     @Autowired MemberRepository memberRepository;
+
     @Test
-    public void 회원가입() throws Exception {
+    void 회원가입() {
         //Given
         Member member = new Member();
-        member.setName("hello");
+        member.setName("spring");
         //When
         Long saveId = memberService.join(member);
         //Then
         Member findMember = memberRepository.findById(saveId).get();
-        assertEquals(member.getName(), findMember.getName());
+        assertThat(member.getName()).isEqualTo(findMember.getName());
     }
     @Test
-    public void 중복_회원_예외() throws Exception {
+    void 중복_회원_예외(){
         //Given
         Member member1 = new Member();
         member1.setName("spring");
